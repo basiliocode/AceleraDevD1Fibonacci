1cb2c4e74397629db0cd72278200a114bd955b8d
Thiago Basilio <basilio.barboza@gmail.com>
2020-07-11 03:18:36 -0300

    Desafio 1 pronto

--- src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -1,15 +1,25 @@
 package br.com.codenation.desafioexe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DesafioApplication {
-
+	static List<Integer> list = new ArrayList<>();
 	public static List<Integer> fibonacci() {
-		return null;
+		int a = 0;
+		int b = 1;
+		int aux;
+		while (a < 350){
+			list.add(a);
+			aux = a; //guardar o valor antes de sobreescrever variavel a
+			a = a + b;
+			b = aux; //recuperando o valor para o proximo loop
+		}
+		return list;
 	}
 
 	public static Boolean isFibonacci(Integer a) {
-		return null;
+		return list.contains(a);
 	}
 
 }
