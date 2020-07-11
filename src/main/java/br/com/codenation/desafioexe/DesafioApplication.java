560853894e9c1297a964cbd5778869e1c4cf8648
Thiago Basilio <basilio.barboza@gmail.com>
2020-07-11 14:20:16 -0300

    Ajustes dno fonte

--- src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DesafioApplication {
-	static List<Integer> list = new ArrayList<>();
+
 	public static List<Integer> fibonacci() {
-		int a = 0;
-		int b = 1;
-		int aux;
-		while (a < 350){
+		List<Integer> list = new ArrayList<>();
+		int a = 0, b = 1, aux;
+		do{
 			list.add(a);
 			aux = a; //guardar o valor antes de sobreescrever variavel a
 			a = a + b;
 			b = aux; //recuperando o valor para o proximo loop
-		}
+		}while (aux < 350);
+
 		return list;
 	}
 
 	public static Boolean isFibonacci(Integer a) {
-		return list.contains(a);
+		return fibonacci().contains(a);
 	}
 
 }
